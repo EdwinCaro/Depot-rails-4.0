1f6ae74e92bac8f96d3824f8da4e58496bed15a2
caroe2014 <edwincaro10@hotmail.com>
2014-08-20 07:33:25 -0500

    Update secret_token.rb

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,4 +9,4 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Depot::Application.config.secret_key_base = 'ee8fbf8527d0996d5d3b1f0e6d9da173932260a696cdf80461b22fda7baac32b8c67504b0ccc468be252382ca6f6f91b6f64f43d7c1557d49bb020c852ea86da'
+Depot::Application.config.secret_key_base = 
